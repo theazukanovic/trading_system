6c38aff11932801963736f6667be599f3c09e0dc
theazukanovic <thea.zukanovic@student.nbi-handelsakademin.se>
2025-09-27 20:13:02 +0200

    Add login loop to program.cs and fix syntax in user.cs
    This makes it possible to log in with email and password. Fixed minor
    syntax issues in user class to make code compile.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-sfas
+﻿using App;
+
+List<User> users = new List<User>();
+
+users.Add(new User("Thea", "t.z@hotmail.se", "thea")); //testanvändare
+User? active_user = null; //om man sätter null så betyder det att är det ingen användare som är selected = utloggad
+
+bool running = true;
+while (running)
+{
+    Console.Clear();
+
+    if (active_user == null)
+    {
+        Console.Write("Email: ");
+        string email = Console.ReadLine();
+        Console.Clear();
+
+        Console.Write("Password: ");
+        string password = Console.ReadLine();
+        Console.Clear();
+
+        foreach (User user in users) //kollar igenom alla användare
+        {
+            if (user.TryLogin(email, password))
+            {
+                active_user = user;
+                break; //sluta leta om vi hittar rätt
+            }
+        }
+        if (active_user == null) // om ingen användare hittas
+        {
+            Console.WriteLine("Wrong email or password");
+            Console.WriteLine("Press space to try again..");
+            Console.ReadLine();
+        }
+    }
+    else
+    {
+        Console.WriteLine("-----Trading system-----");
+    }
+}
--- User.cs
+++ User.cs
@@ -2,19 +2,19 @@ namespace App;
 
 public class User
 {
-  public string Name;
-  public string Email;
-  string _password;
+    public string Name;
+    public string Email;
+    string _password;
 
-  public User(string name, string email, string password)
-  {
-    Name = name;
-    Email = email;
-    _password = password
-  }
+    public User(string name, string email, string password)
+    {
+        Name = name;
+        Email = email;
+        _password = password;
+    }
 
-  public bool TryLogin(string email, string password)
-  {
-    return email == Email && password == _password
-  }
+    public bool TryLogin(string email, string password)
+    {
+        return email == Email && password == _password;
+    }
 }
