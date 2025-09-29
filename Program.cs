7ddc2a095be41ea5335efe8fd86203a4fbe0e249
theazukanovic <thea.zukanovic@student.nbi-handelsakademin.se>
2025-09-29 17:51:57 +0200

    add case 2; browse items from other users
    Allows logged in user to view other users inventory.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ List<Trade> trades = new List<Trade>();
 
 users.Add(new User("Thea", "t.z@hotmail.se", "thea")); //testanvändare
 users.Add(new User("Manuel", "manuel@gmail.com", "hej")); //testanavändare2
+users.Add(new User("Marcus", "marcus@hotmail.com", "hej")); // test3
+users.Add(new User("Malin", "malin@hotmail.se", "malin")); // test 4
+
 User? active_user = null; //om man sätter null så betyder det att är det ingen användare som är selected = utloggad
 
 bool running = true;
@@ -54,7 +57,7 @@ while (running)
         {
             case "1": //Lägg till item på den inloggade användaren
                 Console.Clear();
-                Console.WriteLine("---Upload new item---");
+                Console.WriteLine("---Upload item to trade---");
                 Console.Write("Item name:  ");
                 string name = Console.ReadLine();
                 Console.Write("Description:  ");
@@ -66,6 +69,44 @@ while (running)
 
                 Console.WriteLine("Item added!");
                 Console.WriteLine("Press enter to go back to the menu..");
+                Console.ReadLine();
+                break;
+
+            case "2":
+                Console.Clear();
+                Console.WriteLine("---Browse items from other users---");
+
+                int count = 0; //räknar hur många andra användare vi visar
+
+                //Loopa igenom alla användare
+                for (int i = 0; i < users.Count; i++)
+                {
+                    if (users[i] == active_user)
+                        continue; //hoppa över inloggad användare
+
+                    Console.WriteLine("User" + " " + users[i].Name + " " + "inventory:");
+
+                    if (users[i].Items.Count == 0)
+                    {
+                        Console.WriteLine("There is no items to show..");
+                    }
+                    else
+                    {
+                        foreach (Item item in users[i].Items)
+                        {
+                            Console.WriteLine("User:" + " " + item.Name + ": " + item.Description);
+                        }
+                    }
+
+                    count++; //vi visar en användare
+                    Console.WriteLine();
+                }
+                //om count är 0 = inga andra användare
+                if (count == 0)
+                {
+                    Console.WriteLine("No other users yet");
+                }
+                Console.WriteLine("Press enter to go back to menu");
                 Console.ReadLine();
                 break;
         }
