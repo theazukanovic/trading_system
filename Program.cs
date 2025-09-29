eb3faa779d6ea7393a5776d3f292c6cb505dc6db
theazukanovic <thea.zukanovic@student.nbi-handelsakademin.se>
2025-09-29 13:52:35 +0200

    add case 1: upload item to trade
    Allows logged in users to upload the item they want to trade with name
    and description.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using App;
 
 List<User> users = new List<User>();
+List<Trade> trades = new List<Trade>();
 
 users.Add(new User("Thea", "t.z@hotmail.se", "thea")); //testanvändare
+users.Add(new User("Manuel", "manuel@gmail.com", "hej")); //testanavändare2
 User? active_user = null; //om man sätter null så betyder det att är det ingen användare som är selected = utloggad
 
 bool running = true;
@@ -36,7 +38,7 @@ while (running)
         }
     }
     else
-    {
+    { // borde man kunna se inventory?
         Console.WriteLine("-----Trading system-----");
         Console.WriteLine("1) Upload item to trade");
         Console.WriteLine("2) Browse items from other users");
@@ -47,5 +49,25 @@ while (running)
         Console.WriteLine("7) Log out");
 
         string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1": //Lägg till item på den inloggade användaren
+                Console.Clear();
+                Console.WriteLine("---Upload new item---");
+                Console.Write("Item name:  ");
+                string name = Console.ReadLine();
+                Console.Write("Description:  ");
+                string desc = Console.ReadLine();
+                // skapa ett nytt item objekt med ägare = active_user:
+                Item newItem = new Item(name, desc, active_user);
+                //lägg in item i den inloggade användarens inventory:
+                active_user.Items.Add(newItem);
+
+                Console.WriteLine("Item added!");
+                Console.WriteLine("Press enter to go back to the menu..");
+                Console.ReadLine();
+                break;
+        }
     }
 }
